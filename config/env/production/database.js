5e29aa5ad7cad92226c2742be3098dddb8a5d4a2
davidbarbi3r <davidbarbi3r@gmail.com>
2023-06-20 11:21:44 +0200

    :label: add new content types

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,7 +7,6 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         ca: env('DATABASE_CA', ''),
       },
